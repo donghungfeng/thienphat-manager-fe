592e35b94a9b06609a029571db832aa44ca4f781
TruongThanhTung <truongthanhtung2701.dev@gmail.com>
2025-10-04 20:04:37 +0700

    update

--- src/app/Pages/quan-ly/bang-cham-cong/bao-cao-cong-viec/bao-cao-cong-viec.component.ts
+++ src/app/Pages/quan-ly/bang-cham-cong/bao-cao-cong-viec/bao-cao-cong-viec.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'bao-cao-cong-viec',
@@ -6,5 +8,22 @@ import { Component } from "@angular/core";
   standalone: false
 })
 export class BaoCaoCongViecModal {
-
+  @Input() data: any
+  form: FormGroup;
+  departmentList: any[] = []
+  constructor(
+    private fb: FormBuilder,
+    private modal: NgbActiveModal
+  ) {
+    this.form = this.fb.group({
+      title: ["", {
+        validators: [Validators.required],
+        updateOn: 'change'
+      }],
+      content: [""],
+    });
+  }
+  closeModal(isReturn = null) {
+    this.modal.close(isReturn)
+  }
 }
--- src/app/Pages/quan-ly/bang-cham-cong/giai-trinh-cham-cong/giai-trinh-cham-cong.component.ts
+++ src/app/Pages/quan-ly/bang-cham-cong/giai-trinh-cham-cong/giai-trinh-cham-cong.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'giai-trinh-cham-cong',
@@ -6,5 +8,21 @@ import { Component } from "@angular/core";
   standalone: false
 })
 export class GiaiTrinhChamCongModal {
-  
+  @Input() data: any
+  form: FormGroup;
+  departmentList: any[] = []
+  constructor(
+    private fb: FormBuilder,
+    private modal: NgbActiveModal
+  ) {
+    this.form = this.fb.group({
+      content: ["", {
+        validators: [Validators.required],
+        updateOn: 'change'
+      }],
+    });
+  }
+  closeModal(isReturn = null) {
+    this.modal.close(isReturn)
+  }
 }
